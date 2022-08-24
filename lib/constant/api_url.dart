0bdfa49442be5d627d935aede2c552b90da13719
Rama <adamramadhan340@gmail.com>
2022-08-24 10:34:16 +0700

    base url, endpoint and models

--- lib/constant/api_url.dart
+++ lib/constant/api_url.dart
@@ -1,13 +1,17 @@
 class ApiUrl {
   static String baseUrl = "https://ekskul.id/api/";
+  static String apiKey = "18be70c0-4e4d-44ff-a475-50c51ece99a0";
 
   static String users = "users";
-  static String usersRegistrasi = "user/registrasi";
-  static String usersUpdateProfile = "user/update";
+  static String usersRegistrasi = "users/registrasi";
+  static String usersUpdateProfile = "users/update";
 
   static String latihanMapel = "exercise/data_course";
-  static String latihanPaketSoal = "";
+  static String latihanPaketSoal = "exercise/data_exercise";
   static String latihanKerjakanSoal = "exercise/kerjakan";
   static String latihanSubmitJawaban = "exercise/input_jawaban";
   static String latihanSkor = "exercise/score_result";
+
+  static String banner = "event/list";
+  
 }
